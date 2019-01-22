46be863cb02354723fa7a75bf01e5154867e7e69
Akhil K <akhil.kn@pitsolutions.com>
2019-01-22 13:31:23 +0530

    Code Refactoring

--- index.js
+++ index.js
@@ -7,16 +7,15 @@ const config = {
     "m3u8_path": "https://vodhls-vh.akamaihd.net/i/songs/37/1888137/21532380/21532380_64.mp4/master.m3u8?set-akamai-hls-revision=5&hdnts=st=1548143330~exp=1548161330~acl=/i/songs/37/1888137/21532380/21532380_64.mp4/*~hmac=9957e0ae4a318261fc380e1a04fd986e28344246705da9832527ded1826aadd0",
     "bitrate": "320"
 };
+
 const command = ffmpeg(config.m3u8_path);
 const file_path = `Music/${config.name}.mp3`;
 
 command
-    .on('progress', function (progress) {
-        console.log('Processing: ' + Math.floor(progress.percent) + '% done');
-    })
-    .on('error', (err) => { console.log('An error occurred: ' + err.message); })
     .on('start', () => { console.log('Processing started !'); })
+    .on('progress', (progress) => { console.log('Processing: ' + Math.floor(progress.percent) + '% done'); })
     .on('end', () => { addCover(); })
+    .on('error', (err) => { console.log('An error occurred: ' + err.message); })
     .audioCodec('libmp3lame')
     .audioBitrate(config.bitrate)
     .mergeToFile('temp.mp3', './Music');
